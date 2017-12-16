d0371657975772e8c58980894f2e4b57e018877f
Samuel Carlsson <samuel.carlsson@gmail.com>
2017-12-16 09:45:19 +0100

    16.2

--- 2017/16.py
+++ 2017/16.py
@@ -20,30 +20,35 @@ def dance(program, initial):
             a, b = d.index(pa), d.index(pb)
             d[a], d[b] = d[b], d[a]
     
-    return [initial.index(x) for x in d]
+    return d
+
+def find_loop(program, initial):
+    d = initial.copy()
+    for i in range(1000000000):
+        d = dance(program, d)
+        if d == initial:
+            return i + 1
+    return None
+
 
 ex = """s1,x3/4,pe/b"""
 n = 5
 initial = list(alphabet[0:n])
-m = dance(ex, initial)
 d = initial
+d = dance(ex, d)
 print("".join(d))
-d = [d[x] for x in m]  # fast program 
-print("".join(d))
-d = [d[x] for x in m]  # fast program 
+d = dance(ex, d)
 print("".join(d))
 
+n = 16
+with open('input/16') as f:
+    program = f.read()
 
-#n = 16
-#with open('input/16') as f:
-#    program = f.read()
+initial = list(alphabet[0:n])
+cycle = find_loop(program, initial)
 
-#initial = list(alphabet[0:n])
-#m = dance(program, d)
-
-
-#d = initial.copy()
-#for _ in tqdm(range(1000000000), mininterval=0.5):
-#    d = [d[x] for x in m]  # fast program 
-
-#print(''.join(d))
+d = initial
+for _ in range(1000000000 % cycle):
+    d = dance(program, d)
+    
+print(''.join(d))
