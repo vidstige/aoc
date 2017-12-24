0e6603f1b855a2e82ce4778b1176ec004e2167a3
Samuel Carlsson <samuel.carlsson@gmail.com>
2017-12-24 08:21:30 +0100

    23.2

--- 2017/23.c
+++ 2017/23.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 int main() {
-    int a = 0;
+    int a = 1;
     int b = 0, c = 0, d = 0, e = 0, f = 0, g = 0, h = 0;
     
     b = 65; // set b 65
@@ -17,28 +17,20 @@ int main() {
     }
 
     do {
-        printf("outer\n");
         f = 1; // set f 1
         d = 2; // set d 2
-        do {
-            e = 2; // set e 2
-            do {
-                g = d; // set g d
-                g *= e; // mul g e
-                g -= b; // sub g b
-                // jnz g 2
-                if (g == 0) {
-                    f = 0; // set f 0
+        /*for (d = 2; d < b; d++) {
+            for (e = 2; e < b; e++) {
+                if (d * e == b) {
+                    f = 0;
                 }
-                e -= -1;    // sub e -1
-                g = e;      // set g e
-                g -= b;     // sub g b
-            } while (g != 0); // jnz g -8
-            
-            d -= -1; // sub d -1
-            g = d; // set g d
-            g -= b; // sub g b
-        } while (g != 0);     // jnz g -13
+            }
+        }*/
+        for (d = 2; d < b/2; d++) {
+            if (b % d == 0) {
+                f = 0;
+            }
+        }
         
         if (f == 0) { // jnz f 2
             printf("b: %d\n", b);
@@ -47,6 +39,7 @@ int main() {
         g = b; // set g b
         g -= c; // sub g c
         if (g != 0) {
+            printf("OK!\n");
             b -= -17;
         }
     } while (g != 0);
