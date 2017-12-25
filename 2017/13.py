9f8c449704e64017a771a57577aca447bf6227c2
Samuel Carlsson <samuel.carlsson@gmail.com>
2017-12-25 17:00:46 +0100

    13.2

--- 2017/13.py
+++ 2017/13.py
@@ -1,79 +1,17 @@
-import itertools
-
-ex = """0: 3
-1: 2
-4: 4
-6: 4"""
-
-def parse(data):
-    ranges = {}
-    for line in data.splitlines():
-        depth, range1 = line.split(': ')
-        depth = int(depth)
-        ranges[depth] = int(range1)
-    return ranges
-
-
-def severity(interceptions, ranges):
-    s = 0
-    for i in interceptions:
-        s += i * ranges[i]
-    return s
-
-def step(t, ranges, scanners):
-    for s in scanners:
-        direction = 1 if (t // (ranges[s]-1)) % 2 == 0 else -1
-        scanners[s] += direction
-
-#next_initial = None
-def run(ranges, s0=None, delay=0):
-    #global next_initial
-    #if delay == 0:
-    #    # all scanners start at 0
-    #    scanners = {d: 0 for d in ranges}
-    #else:
-    #    scanners = next_initial
-    #
-    #next_initial = scanners.copy()
-    #step(delay, ranges, next_initial)
-    scanners = s0
-    #if scanners is None:
-    #    scanners = {d: 0 for d in ranges}
-
-    t = delay
-
-    interceptions = []
-    for d in range(max(ranges)+1):
-        # 1. intercept
-        if scanners.get(d) == 0:
-            interceptions.append(d)
-
-        # 2. step scanners
-        step(t, ranges, scanners)
-        t += 1
-
-    return interceptions
-
-def first_free(ranges):
-    scanners = {d: 0 for d in ranges}
-    for delay in itertools.count():
-        interceptions = run(ranges, scanners.copy(), delay=delay)
-        if not interceptions:
-            print()
-            return delay
-        if delay % 13 == 0:
-            print('\r{}'.format(delay), end="")
-
-        step(delay, ranges, scanners)
-
-    return None
-
-#data = parse(ex)
-#print(severity(run(data), data))
-#print(first_free(data))
+from itertools import count
 
 with open('input/13') as f:
-    data = parse(f.read())
+    lines = [line.split(': ') for line in f.readlines()]
 
-#print(severity(run(data), data))
-print(first_free(data))
+heights = {int(pos): int(height) for pos, height in lines}
+
+def scanner(height, time):
+    offset = time % ((height - 1) * 2)
+
+    return 2 * (height - 1) - offset if offset > height - 1 else offset
+
+part1 = sum(pos * heights[pos] for pos in heights if scanner(heights[pos], pos) == 0)
+part2 = next(wait for wait in count() if not any(scanner(heights[pos], wait + pos) == 0 for pos in heights))
+
+print(part1)
+print(part2)
