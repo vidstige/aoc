7bcdb5fe130581bc7fb708b78a535bab3261ac4d
Samuel Carlsson <samuel.carlsson@gmail.com>
2018-12-08 08:03:52 +0100

    8.2

--- 2018/8.py
+++ 2018/8.py
@@ -10,6 +10,16 @@ def metadata(data):
     for _ in range(metadata_count):
         yield data.pop(0)
 
+def value(data):
+    n = data.pop(0)
+    m = data.pop(0)
+
+    childs = [value(data) for _ in range(n)] if n else []
+    metadata = [data.pop(0) for _ in range(m)]
+    if n:
+        return sum(childs[i-1] for i in metadata if i-1 in range(len(childs)))
+    return sum(metadata)
 
 data = load('input/8')
-print(sum(metadata(data)))
+#print(sum(metadata(data)))
+print(value(data))
