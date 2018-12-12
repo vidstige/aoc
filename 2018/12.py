7a14b4cf33d7c2dd7ab79249bd032e2021ab2455
Samuel Carlsson <samuel.carlsson@gmail.com>
2018-12-12 17:58:07 +0100

    12

---
+++ 2018/12.py
@@ -0,0 +1,43 @@
+
+def load(filename):
+    with open(filename) as f:
+        initial = f.readline()[len('initial state: '):].rstrip()
+        f.readline()
+        
+        rules = []
+        for line in f.readlines():
+            left = line[:5]
+            right = line.rstrip()[-1]
+            rules.append((left, right))
+    
+        return initial, rules
+
+def generation(state, rules):
+    # zero pad
+    index, state = state
+    s = "...{}...".format(state)
+    result = [c for c in s]
+    #result = ['.' for _ in s]
+    for i in range(2, len(s)-2):
+        #print('  ', s[i-2:i+3])
+        for pattern, produce in rules:
+            if s[i-2:i+3] == pattern:
+                result[i] = produce
+    
+    res = ''.join(result)
+    return index + res.index('#') - 3, res.strip('.')
+
+
+def pot_numbers(state):
+    offset, s = state
+    for i, c in enumerate(s):
+        if c == '#':
+            yield i + offset
+
+initial, rules = load('input/12')
+state = 0, initial
+for g in range(20):
+    print(g, state)
+    state = generation(state, rules)
+
+print(sum(pot_numbers(state)))
