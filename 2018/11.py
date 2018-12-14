73e6b85b1672253482680406bb628bc7494ec332
Samuel Carlsson <samuel.carlsson@gmail.com>
2018-12-14 19:05:13 +0100

    11.2

--- 2018/11.py
+++ 2018/11.py
@@ -1,24 +1,34 @@
+from tqdm import tqdm
+from functools import lru_cache
+
 def hundreds(x):
     return int(str(x // 100)[-1])
-    
+
+
 def cell(serial, p):
     x, y = p
     rack_id = x + 10
     level = (rack_id * y + serial) * rack_id
     return hundreds(level) - 5
 
+
 def grid(w, h):
     for y in range(w):
         for x in range(h):
             yield x, y
 
-def power_levels(serial, size=300, kernel=3):
-    levels = {}
-    kernel = 3
+def compute_power_grid(serial, size=300):
+    power_grid = {}
+    for x, y in grid(size, size):
+        p = x+1, y+1
+        power_grid[p] = cell(serial, p)
+    return power_grid
+
+def power_levels(levels, power_grid, kernel=3, size=300):
     for x, y in grid(size - kernel, size - kernel):
-        p = x + 1, y + 1
-        levels[p] = sum(cell(serial, (x + a + 1, y + b + 1)) for a, b in grid(3, 3))
+        levels[(x+1, y+1, kernel)] = sum(power_grid[(x+1 + a, y+1 + b)] for a, b in grid(kernel, kernel))
     return levels
+
 
 def highest(levels):
     return max((l, c) for c, l in levels.items())
@@ -26,8 +36,20 @@ def highest(levels):
 #print(cell(71, (101,153)))
 #print(18, highest(power_levels(18)))
 #print(42, highest(power_levels(42)))
-print(1955, highest(power_levels(1955)))
 
+# 1
+#print(1955, highest(power_levels(1955)))
 
+def search():
+levels = {}
+power_grid = compute_power_grid(serial=1955)
+size = 300
+for kernel in tqdm(range(1, 300 + 1)):
+    left = kernel // 2
+    right = kernel - left
+    for a, b in grid(size - kernel, size - kernel):
+        x, y = a + 1, b + 1
+        print(x, y, kernel)
     
-
+#power_levels(levels, power_grid, 3)
+#print(highest(levels))
