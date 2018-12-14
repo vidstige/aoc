fc235c0156459e4a7593e864197bbc764e702b18
Samuel Carlsson <samuel.carlsson@gmail.com>
2018-12-14 19:05:02 +0100

    14.2

--- 2018/14.py
+++ 2018/14.py
@@ -7,9 +7,27 @@ def main(scoreboard, after, last=10):
         elves = [(e + int(scoreboard[e]) + 1) % len(scoreboard) for e in elves]
     return scoreboard[after:after+last]
 
-print(main('37', 5), '0124515891')
-print(main('37', 9), '5158916779')
-print(main('37', 18), '9251071085')
-print(main('37', 2018), '5941429882')
+def search(scoreboard, match):
+    elves = [0, 1]
+    scores = [int(rs) for rs in scoreboard]
+    while True:
+        scores.extend(int(r) for r in str(sum(scores[e] for e in elves)))
+        elves = [(e + scores[e] + 1) % len(scores) for e in elves]
 
-print(main('37', 920831))
+        if match in ''.join(str(x) for x in scores[-len(match)-2:]):
+            return len(scores) - len(match)
+
+
+#print(main('37', 5), '0124515891')
+#print(main('37', 9), '5158916779')
+#print(main('37', 18), '9251071085')
+#print(main('37', 2018), '5941429882')
+
+#print(main('37', 920831))
+
+print(search('37', '01245'), 5)
+print(search('37', '51589'), 9)
+print(search('37', '92510'), 18)
+print(search('37', '59414'), 2018)
+
+print(search('37', '920831'))
