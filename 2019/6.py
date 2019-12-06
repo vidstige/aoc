771e399bbed7e85f13b4e3d0361652f8d0156d0b
Samuel Carlsson <samuel.carlsson@gmail.com>
2019-12-06 17:00:16 +0100

    6.2

--- 2019/6.py
+++ 2019/6.py
@@ -1,5 +1,31 @@
 from collections import defaultdict
 
+ex1 = """COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L"""
+
+ex2 = """COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN"""
+
 def load():
     with open('input/6') as f:
         return f.read()
@@ -17,16 +43,26 @@ def search(orbits, name, d):
     names = orbits[name]
     return sum(search(orbits, n, d + 1) for n in names) + d
 
-data = """COM)B
-B)C
-C)D
-D)E
-E)F
-B)G
-G)H
-D)I
-E)J
-J)K
-K)L"""
 
-print(search(parse(load()), 'COM', 0))
+def edges(data):
+    return [frozenset(line.split(')')) for line in data.splitlines()]
+
+
+def distance(edges, start, stop):
+    q = [(start, 0)]
+    visited = set()
+    while q:
+        node, d = q.pop(0)
+        if node == stop:
+            return d - 2
+        visited.add(node)
+        for edge in edges:
+            if node in edge:
+                q.extend((n, d + 1) for n in edge - frozenset(node) - visited)
+    
+    return None
+
+
+
+#print(search(parse(load()), 'COM', 0))
+print(distance(edges(load()), 'YOU', 'SAN'))
