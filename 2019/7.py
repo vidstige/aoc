beb8322ef3d8af724d6cfde0d05f9ea9234b1b21
Samuel Carlsson <samuel.carlsson@gmail.com>
2019-12-07 13:02:52 +0100

    7.2

--- 2019/7.py
+++ 2019/7.py
@@ -1,6 +1,6 @@
-from itertools import permutations
+from itertools import permutations, cycle
 from pathlib import Path
-from intvm import run
+from intvm import Intcode
 
 def load():
     no = Path(__file__).stem
@@ -12,20 +12,48 @@ def parse(data):
 def amplifier(program, settings):
     i = 0
     for setting in settings:
-        amp = program.copy()
-        outputs = run(amp, data=[i, setting])
-        i = outputs[0]
-    return outputs[0]
+        amp = Intcode(program.copy())
+        outputs = amp.run(data=[i, setting])
+        i = outputs
+    return outputs
+
+def amplifier_feedback(program, settings):
+    amps = [Intcode(program.copy()) for _ in settings]
+    for amp, setting in zip(amps, settings):
+        amp.write(setting)
+
+    last_e = None
+    amps[0].write(0)
+    i = 0
+    for index, amp in cycle(enumerate(amps)):
+        #print(i, index, len([a for a in amps if not a.is_terminated()]))
+        output = amp.run()
+        if output is not None:
+            amps[(index+1) % len(amps)].write(output)
+            if index == len(settings) - 1:
+                last_e = output
+        if all(a.is_terminated() for a in amps):
+            break
+        i += 1
+    #print(last_e)
+    return last_e
 
 def optimize(program):
-    settings = permutations(range(5))
-    return max(amplifier(program, setting) for setting in settings)
+    settings = permutations(range(5, 10))
+    return max((amplifier_feedback(program, setting), setting) for setting in settings)
 
 ex1=[3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0]
 ex2=[3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0]
 ex3=[3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0]
 
-print(optimize(ex1))
-print(optimize(ex2))
-print(optimize(ex3))
+ex4=[3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
+ex5=[3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10]
+
+#print(optimize(ex1))
+#print(optimize(ex2))
+#print(optimize(ex3))
+#print(optimize(ex4))
+#print(optimize(ex5))
 print(optimize(parse(load())))
+
+# bad: 9124423
--- 2019/intvm.py
+++ 2019/intvm.py
@@ -2,72 +2,87 @@ def load(day):
     with open('input/{day}'.format(day=day)) as f:
         return [int(x) for x in f.read().split(',')]
 
-def parameter(i, ip, program, modes):
-    value = program[ip + i]
-    if modes[i] == 0:
-        return program[value]
-    return value
+class Intcode:
+    def __init__(self, program):
+        self.program = program
+        self.ip = 0
+        self.data = []
 
-def run(program, data=None):
-    ip = 0
-    out = []
-    while True:
-        opcode = program[ip] % 100
-        pm = program[ip] // 100
-        ip += 1
-        modes = []
-        for _ in range(3):
-            modes.append(pm % 10)
-            pm = pm // 10
+    def write(self, data):
+        self.data.append(data)
 
-        if opcode == 1:
-            a = parameter(0, ip, program, modes)
-            b = parameter(1, ip, program, modes)
-            destination = program[ip + 2]
-            program[destination] = a + b
-            ip += 3
-        elif opcode == 2:
-            a = parameter(0, ip, program, modes)
-            b = parameter(1, ip, program, modes)
-            destination = program[ip + 2]
-            program[destination] = a * b
-            ip += 3
-        elif opcode == 3:
-            destination = program[ip]
-            program[destination] = data.pop()
-            ip += 1
-        elif opcode == 4:
-            source = program[ip]
-            out.append(program[source])
-            ip += 1
-        elif opcode == 5:
-            condition = parameter(0, ip, program, modes)
-            jump = parameter(1, ip, program, modes)
-            if condition:
-                ip = jump
+    def _parameter(self, i, modes):
+        value = self.program[self.ip + i]
+        if modes[i] == 0:
+            return self.program[value]
+        return value
+
+    def run(self):
+        while True:
+            opcode = self.program[self.ip] % 100
+            pm = self.program[self.ip] // 100
+            self.ip += 1
+            modes = []
+            for _ in range(3):
+                modes.append(pm % 10)
+                pm = pm // 10
+
+            if opcode == 1:
+                a = self._parameter(0, modes)
+                b = self._parameter(1, modes)
+                destination = self.program[self.ip + 2]
+                self.program[destination] = a + b
+                self.ip += 3
+            elif opcode == 2:
+                a = self._parameter(0, modes)
+                b = self._parameter(1, modes)
+                destination = self.program[self.ip + 2]
+                self.program[destination] = a * b
+                self.ip += 3
+            elif opcode == 3:
+                destination = self.program[self.ip]
+                self.program[destination] = self.data.pop(0)
+                self.ip += 1
+            elif opcode == 4:
+                #ource = self.program[self.ip]
+                #return self.program[source]
+                #out.append(self.program[source])
+                tmp = self._parameter(0, modes)
+                self.ip += 1
+                return tmp
+            elif opcode == 5:
+                condition = self._parameter(0, modes)
+                jump = self._parameter(1, modes)
+                if condition:
+                    self.ip = jump
+                else:
+                    self.ip += 2
+            elif opcode == 6:
+                condition = self._parameter(0, modes)
+                jump = self._parameter(1, modes)
+                if not condition:
+                    self.ip = jump
+                else:
+                    self.ip += 2
+            elif opcode == 7:
+                a = self._parameter(0, modes)
+                b = self._parameter(1, modes)
+                destination = self.program[self.ip + 2]
+                self.program[destination] = 1 if a < b else 0
+                self.ip += 3
+            elif opcode == 8:
+                a = self._parameter(0, modes)
+                b = self._parameter(1, modes)
+                destination = self.program[self.ip + 2]
+                self.program[destination] = 1 if a == b else 0
+                self.ip += 3
+            elif opcode == 99:
+                break
             else:
-                ip += 2
-        elif opcode == 6:
-            condition = parameter(0, ip, program, modes)
-            jump = parameter(1, ip, program, modes)
-            if not condition:
-                ip = jump
-            else:
-                ip += 2
-        elif opcode == 7:
-            a = parameter(0, ip, program, modes)
-            b = parameter(1, ip, program, modes)
-            destination = program[ip + 2]
-            program[destination] = 1 if a < b else 0
-            ip += 3
-        elif opcode == 8:
-            a = parameter(0, ip, program, modes)
-            b = parameter(1, ip, program, modes)
-            destination = program[ip + 2]
-            program[destination] = 1 if a == b else 0
-            ip += 3
-        elif opcode == 99:
-            break
-        else:
-            raise ValueError('bad opcode: {}'.format(opcode))
-    return out
+                raise ValueError('bad opcode: {}'.format(opcode))
+        # terminated
+        self.ip -= 1
+        return None
+
+    def is_terminated(self):
+        return self.program[self.ip] % 100 == 99
