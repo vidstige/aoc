e3fa11eb7029d649a5890692aeb90798dce00fe5
Samuel Carlsson <samuel.carlsson@gmail.com>
2020-12-01 20:40:53 +0100

    cleanup

--- 2020/1.rs
+++ 2020/1.rs
@@ -2,10 +2,11 @@ use std::io::{self, BufRead};
 
 fn main() {
     let stdin = io::stdin();
-    let tab: Vec<i32> = stdin.lock().lines().into_iter().map(|line| line.unwrap().parse::<i32>().unwrap()).collect();
+    let lines = stdin.lock().lines();
+    let tab: Vec<i32> = lines.map(|line| line.unwrap().parse::<i32>().unwrap()).collect();
 
     for i in 0..tab.len() {
-        for j in i..tab.len() {
+        for j in (i+1)..tab.len() {
             if tab[i] + tab[j] == 2020 {
                 println!("{}", tab[i] * tab[j]);
             }
