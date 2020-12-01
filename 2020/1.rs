d5971b0543b1ea87db1222b28a8aa793a37201be
Samuel Carlsson <samuel.carlsson@gmail.com>
2020-12-01 06:44:44 +0100

    rust.1

--- 2020/1.rs
+++ 2020/1.rs
@@ -1,3 +1,14 @@
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let tab: Vec<i32> = stdin.lock().lines().into_iter().map(|line| line.unwrap().parse::<i32>().unwrap()).collect();
+
+    for i in 0..tab.len() {
+        for j in i..tab.len() {
+            if tab[i] + tab[j] == 2020 {
+                println!("{}", tab[i] * tab[j]);
+            }
+        }
+    }
 }
