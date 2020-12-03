fe63ab2b7fd6d3e3825e7ac348ea6a815fc777a1
Samuel Carlsson <samuel.carlsson@gmail.com>
2020-12-03 06:12:18 +0100

    3.2

--- 2020/3.py
+++ 2020/3.py
@@ -3,14 +3,25 @@ import sys
 def parse(f):
     return [line.strip() for line in f]
 
+def count(trees, slope):
+    x, y = 0, 0
+    n = 0
+    while y < len(trees):
+        if trees[y][x % len(trees[y])] == '#':
+            n += 1
+        x, y = x + slope[0], y + slope[1]
+    return n
+
 trees = parse(sys.stdin)
-print(trees)
-x, y = 0, 0
-slope = 3, 1
-n = 0
-while y < len(trees):
-    if trees[y][x % len(trees[y])] == '#':
-        n += 1
-    x, y = x + slope[0], y + slope[1]
-print(n)
+slopes = [
+    (1, 1),
+    (3, 1),
+    (5, 1),
+    (7, 1),
+    (1, 2),
+]
+p = 1
+for slope in slopes:
+    p *= count(trees, slope)
+print(p)
         
