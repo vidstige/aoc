e062c5d4a5e2ccfbb066506b1bbff58fc51692d8
Samuel Carlsson <samuel.carlsson@gmail.com>
2020-12-11 20:48:24 +0100

    11

--- 2020/11.py
+++ 2020/11.py
@@ -22,15 +22,36 @@ def find_neighbours(key, layout):
     x, y = key
     return [layout[(x+dx, y + dy)] for dx, dy in NEIGHBOURS if (x + dx, y + dy) in layout]
 
-def step_cell(value, neighbours):
+def find_visible(key, layout):
+    for dx, dy in NEIGHBOURS:
+        x, y = key
+        x += dx
+        y += dy
+        while (x, y) in layout:
+            if layout[(x, y)] in '#L':
+                yield layout[(x, y)]
+                break
+            x += dx
+            y += dy
+
+def step_cell1(key, value, layout):
+    neighbours = find_neighbours(key, layout)
     if value == 'L' and '#' not in neighbours:
         return '#'
     if value == '#' and len([n for n in neighbours if n == '#']) >= 4:
         return 'L'
     return value
 
+def step_cell2(key, value, layout):
+    visible = list(find_visible(key, layout))
+    if value == 'L' and '#' not in visible:
+        return '#'
+    if value == '#' and len([n for n in visible if n == '#']) >= 5:
+        return 'L'
+    return value
+
 def step(layout):
-    return {key: step_cell(value, find_neighbours(key, layout)) for key, value in layout.items()}
+    return {key: step_cell2(key, value, layout) for key, value in layout.items()}
 
 def print_layout(layout):
     x0, y0 = min(layout)
