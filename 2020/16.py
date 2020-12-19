b59fc0b7593667134b03a1e87fdf9e65c0b5a0ba
Samuel Carlsson <samuel.carlsson@gmail.com>
2020-12-19 13:13:39 +0100

    16.2

--- 2020/16.py
+++ 2020/16.py
@@ -11,18 +11,18 @@ def segments(f):
     return parts
 
 def parse_field(field):
-    _name, ranges = field.split(':')
+    name, ranges = field.split(':')
     splat = [r.split('-', 1) for r in ranges.split('or')]
-    return [range(int(a), int(b) + 1) for a, b in splat]
+    return name, tuple(range(int(a), int(b) + 1) for a, b in splat)
 
 def parse_ticket(raw):
     return tuple([int(i) for i in raw.split(',')])
 
 def parse(f):
-    fields, my, tickets = segments(f)
+    fields, me, tickets = segments(f)
     return (
-        [parse_field(field) for field in fields],
-        None,
+        dict(parse_field(field) for field in fields),
+        parse_ticket(me[1]),
         [parse_ticket(ticket) for ticket in tickets[1:]]
     )
 
@@ -30,13 +30,49 @@ def matches(value, field):
     return any(value in r for r in field)
 
 def invalid(ticket, fields):
+    result = []
     for value in ticket:
-        if not any(matches(value, field) for field in fields):
-            yield value
-    return []
+        if not any(matches(value, field) for field in fields.values()):
+            result.append(value)
+    return result
 
-fields, _me, tickets = parse(sys.stdin)
-all_invalid = []
-for ticket in tickets:
-    all_invalid.extend(invalid(ticket, fields))
-print(sum(all_invalid))
+def is_valid(ticket, fields):
+    return not invalid(ticket, fields)
+
+def field_valid(ticket_value, field):
+    """Checks if field is valid at index for ticket"""
+    return any(ticket_value in r for r in field)
+
+def search(tickets, fields):
+    candidates = [set(fields) for _ in fields]
+    for ticket in tickets:
+        for value, candidate in zip(ticket, candidates):
+            for name, field in fields.items():
+                if not field_valid(value, field):
+                    candidate.discard(name)
+    
+    # find singles
+    singles = {next(iter(c)) for c in candidates if len(c) == 1}
+    while singles < set(fields):
+        for candidate in candidates:
+            if not candidate <= singles:
+                candidate -= singles
+        singles = {next(iter(c)) for c in candidates if len(c) == 1}
+    return [next(iter(c)) for c in candidates]
+
+fields, me, tickets = parse(sys.stdin)
+tickets.append(me)
+valid_tickets = [ticket for ticket in tickets if is_valid(ticket, fields)]
+#print(valid_tickets)
+assignment = search(valid_tickets, fields)
+print(assignment)
+a = 1
+for name, value in zip(assignment, me):
+    if name.startswith('departure'):
+        a *= value
+print(a)
+
+#all_invalid = []
+#for ticket in tickets:
+#    all_invalid.extend(invalid(ticket, fields))
+#print(sum(all_invalid))
