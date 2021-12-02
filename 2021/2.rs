fdfecad2c7d66f68f6b152f0c8a3fbdc91cd880d
Samuel Carlsson <samuel.carlsson@volumental.com>
2021-12-02 08:49:09 +0100

    2b

--- 2021/2.rs
+++ 2021/2.rs
@@ -3,22 +3,26 @@ use std::io::{self, BufRead};
 struct Position {
     x: i32,
     depth: i32,
+    aim: i32,
 }
 
 fn main() {
     let stdin = io::stdin();
     let lines = stdin.lock().lines();
     
-    let mut p = Position{x: 0, depth: 0};
+    let mut p = Position{x: 0, depth: 0, aim: 0};
     for maybe_line in lines {
         let line = maybe_line.unwrap();        
         let mut parts = line.split(" ");
         let command = parts.next().unwrap();
         let amount: i32 = parts.next().unwrap().parse().unwrap();
         match command.as_ref() {
-            "forward" => p.x += amount,
-            "down" => p.depth += amount,
-            "up" => p.depth -= amount,
+            "forward" => {
+                p.x += amount;
+                p.depth += p.aim * amount;
+            }
+            "down" => p.aim += amount,
+            "up" => p.aim -= amount,
             _ => println!("unknown command: {}", command),
         }
       }
