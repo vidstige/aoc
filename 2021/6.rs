519f5fe1ac0317d059638bf1c8b32e307bc2c967
Samuel Carlsson <samuel.carlsson@volumental.com>
2021-12-06 09:15:05 +0100

    6b

--- 2021/6.rs
+++ 2021/6.rs
@@ -1,8 +1,10 @@
 use std::io::{self, BufRead};
 
+// 2^(d/7)
+
 fn main() {
     let stdin = io::stdin();
-    let mut fishes: Vec<i32> = stdin
+    let initial: Vec<usize> = stdin
         .lock()
         .lines()
         .map(|line| line.unwrap())
@@ -10,23 +12,24 @@ fn main() {
         .map(|word| word.parse().unwrap())
         .collect();
     
-    let days = 80;
-    for _day in 0..days {
-        let mut new = 0;
-        for fish in &mut fishes {
-            if *fish == 0 {
-                *fish = 6;
-                new += 1;
-            } else {
-                *fish -=1;
-            }
-        }
-        for _ in 0..new {
-            fishes.push(8);
-        }
+    let days = 256;
+    let mut cycle: [i64; 9] = [0; 9];
+    for fish in initial {
+        cycle[fish] += 1;
     }
-    /*for fish in fishes {
-        println!("{}", fish);
-    }*/
-    println!("{}", fishes.len());
+
+    for _day in 0..days {
+        let mut dc: [i64; 9] = [0; 9];
+        for i in 0..(9-1) {
+            dc[i] += cycle[i + 1];
+            dc[i + 1] -= cycle[i + 1];
+        }
+        dc[0] -= cycle[0];
+        dc[8] += cycle[0]; // every fish with 0 spawn a new fish with 8
+        dc[6] += cycle[0]; // all fishes with 0 resets to 6
+        for i in 0..9 {
+            cycle[i] += dc[i];
+        }        
+    }
+    println!("{:?}", cycle.iter().map(|x| *x as i64).sum::<i64>());
 }
