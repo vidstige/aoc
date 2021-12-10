74deb6b6fbbe5c905fe8e13762bc41e7c1a04292
Samuel Carlsson <samuel.carlsson@volumental.com>
2021-12-10 22:20:38 +0100

    10b

--- 2021/10.rs
+++ 2021/10.rs
@@ -1,6 +1,16 @@
 use std::io::{self, BufRead};
 
-fn parse(line: String) -> Option<char> {
+fn points_for(c: char) -> usize {
+    match c {
+        ')' => 1,
+        ']' => 2,
+        '}' => 3,
+        '>' => 4,
+        _ => 0,
+    }
+}
+
+fn parse(line: String) -> Option<usize> {
     let opening = "([{<";
     let closing = ")]}>";
     let mut stack = Vec::new();
@@ -13,22 +23,22 @@ fn parse(line: String) -> Option<char> {
             let open_index = opening.chars().position(|c| c == open).unwrap();
             let expected = closing.chars().nth(open_index).unwrap();
             if c != expected {
-                return Some(c);
+                return None;
             }
         }
     }
-    None
+    stack.reverse();
+    let mut score = 0;
+    for character in stack {
+        let open_index = opening.chars().position(|c| c == character).unwrap();
+        let expected = closing.chars().nth(open_index).unwrap();
+
+        score *= 5;
+        score += points_for(expected);
+    }
+    return Some(score);
 }
 
-fn score(c: char) -> usize {
-    match c {
-        ')' => 3,
-        ']' => 57,
-        '}' => 1197,
-        '>' => 25137,
-        _ => 0,
-    }
-}
 
 fn main() {
     let stdin = io::stdin();
@@ -37,6 +47,7 @@ fn main() {
         .lines()
         .map(|line| line.unwrap());
 
-    let total: usize = lines.map(parse).flatten().map(score).sum();
-    println!("{}", total);
+    let mut scores: Vec<usize> = lines.map(parse).flatten().collect();
+    scores.sort();
+    println!("{}", scores[scores.len() / 2]);
 }
