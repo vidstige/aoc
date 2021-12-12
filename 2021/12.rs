099c00554d231fc4d830f55aa33a1a979fd9efa8
Samuel Carlsson <samuel.carlsson@volumental.com>
2021-12-12 11:39:32 +0100

    12b

--- 2021/12.rs
+++ 2021/12.rs
@@ -22,6 +22,36 @@ fn is_big(cave_id: &String) -> bool {
     return &cave_id.to_uppercase() == cave_id;
 }
 
+fn frequency(v: &Vec<&String>) -> HashMap<String, usize> {
+    let mut f = HashMap::new();
+    for s in v {
+        *f.entry(s.to_string()).or_default() += 1;
+    }
+    f
+}
+
+fn is_acceptable(path: &Vec<String>) -> bool {
+    let small = path.iter().filter(|ci| !is_big(ci)).collect();
+    let freq = frequency(&small);
+    // small caves may only be visited once, except one cave
+    // may be visited twice
+    if freq.values().filter(|c| **c == 2).count() > 1 {
+        return false;
+    }
+    // no cave may be visited _more_ than once
+    if freq.values().any(|c| *c > 2) {
+        return false;
+    }
+    // start & end can only be visited at most once
+    if freq.get(&"start".to_string()).unwrap_or(&0) > &1 {
+        return false;
+    }
+    if freq.get(&"end".to_string()).unwrap_or(&0) > &1 {
+        return false;
+    }
+    true
+}
+
 fn search(map: &HashMap<String, Vec<String>>) -> usize{
     let mut queue = LinkedList::new();
     queue.push_back(("start".to_string(), vec!["start".to_string()]));
@@ -33,10 +63,10 @@ fn search(map: &HashMap<String, Vec<String>>) -> usize{
             count += 1;
         } else {
             for adjecent in map.get(&cave_id).unwrap_or(&Vec::new()) {
-                if is_big(adjecent) || !path.contains(adjecent) {
-                    let mut p = path.clone();
-                    p.push(adjecent.clone());
-                    queue.push_back((adjecent.to_string(), p));
+                let mut p = path.clone();
+                p.push(adjecent.clone());
+                if is_acceptable(&p) {
+                    queue.push_back((adjecent.to_string(), p));      
                 }
             }
         }
