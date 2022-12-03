a1b663f16d034003420253cfd2f872661e4c7028
Samuel Carlsson <samuel.carlsson@gmail.com>
2022-12-03 08:59:11 +0100

    3.2

--- 2022/3.rs
+++ 2022/3.rs
@@ -1,6 +1,7 @@
 use std::io::{self, BufRead};
 use std::collections::HashSet;
-
+use itertools::Itertools;
+ 
 fn priority(c: &char) -> i32 {
     let items = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     items.find(*c).unwrap() as i32 + 1
@@ -11,13 +12,22 @@ fn main() {
     let lines = stdin.lock().lines().map(|line| line.unwrap());
 
     let mut sum = 0;
-    for line in lines {
-        let mid = line.len() / 2;
-        let a: HashSet<_> = line.chars().take(mid).collect();
-        let b: HashSet<_> = line.chars().skip(mid).collect();
-        let common = a.intersection(&b);
+    for (a, b, c) in lines.tuples() {
+        let rucksack_a: HashSet<_> = a.chars().collect();
+        let rucksack_b: HashSet<_> = b.chars().collect();
+        let rucksack_c: HashSet<_> = c.chars().collect();
         
-        sum += common.map(|c| priority(c)).sum::<i32>();
+        let mut badge: HashSet<char> = rucksack_a.intersection(&rucksack_b).map(|c| *c).collect();
+        badge = badge.intersection(&rucksack_c).map(|c| *c).collect();
+        sum += badge.iter().map(|b| priority(b)).sum::<i32>();
+
+        //let mid = line.len() / 2;
+        //let a: HashSet<_> = line.chars().take(mid).collect();
+        //let b: HashSet<_> = line.chars().skip(mid).collect();
+        //let common = a.intersection(&b);
+        
+        //sum += common.map(|c| priority(c)).sum::<i32>();
+        //println!("{}", a);
     }
     println!("{}", sum);
 }
