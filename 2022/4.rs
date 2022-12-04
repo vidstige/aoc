bc4c947f4a9abd222b53d67a54eca137f8db9f3b
Samuel Carlsson <samuel.carlsson@gmail.com>
2022-12-04 09:42:55 +0100

    4.2

--- 2022/4.rs
+++ 2022/4.rs
@@ -12,19 +12,29 @@ fn contains(a: &Range<i32>, b: &Range<i32>) -> bool {
     a.contains(&b.start) && a.contains(&(b.end - 1)) || b.contains(&a.start) && b.contains(&(a.end - 1))
 }
 
+fn overlaps(a: &Range<i32>, b: &Range<i32>) -> bool {
+    (a.start < b.end) && (a.end > b.start)
+}
+
 fn main() {
     let stdin = io::stdin();
     let lines = stdin.lock().lines().map(|line| line.unwrap());
 
-    let mut count = 0;
+    let mut contained = 0;
+    let mut overlapping = 0;
+
     for line in lines {
         let mut split = line.split(",");
         let a = parse_assignment(split.next().unwrap());
         let b = parse_assignment(split.next().unwrap());
-        //println!("{} - {}, {} - {}: {}", a.start, a.end - 1, b.start, b.end - 1, contains(&a, &b));
+        //println!("{} - {}, {} - {}: {}", a.start, a.end - 1, b.start, b.end - 1, overlaps(&a, &b));
         if contains(&a, &b) {
-            count += 1;
+            contained += 1;
+        }
+        if overlaps(&a, &b) {
+            overlapping += 1;
         }
     }
-    println!("{}", count);
+    println!("{}", contained);
+    println!("{}", overlapping);
 }
