04fb8c73971b7d3904b649dae324193b3053fce2
Samuel Carlsson <samuel.carlsson@gmail.com>
2023-12-01 10:32:23 +0100

    1b

--- 2023/1.py
+++ 2023/1.py
@@ -1,10 +1,22 @@
 import sys
 import re
 
+NUMBERS = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+PATTERN_FORWARD = '|'.join(['\d'] + NUMBERS)
+PATTERN_BACKWARDS = '|'.join(['\d'] + [number[::-1] for number in NUMBERS])
+
+
+def toint(value: str) -> str:
+    try:
+        return str(NUMBERS.index(value) + 1)
+    except ValueError:
+        return value
+
 
 def calibration(line: str) -> int:
-    numbers = re.findall('\d', line)
-    return int(numbers[0] + numbers[-1])
+    forward = re.findall(PATTERN_FORWARD, line)
+    backwards = re.findall(PATTERN_BACKWARDS, line[::-1])
+    return int(toint(forward[0]) + toint(backwards[0][::-1]))
 
-lines = [line.rstrip() for line in sys.stdin]
-print(sum(calibration(line) for line in lines))    
+
+print(sum(calibration(line) for line in sys.stdin))
