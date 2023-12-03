5b5f91599cdeb304b30d2cc8aa0e09bf1ead8140
Samuel Carlsson <samuel.carlsson@gmail.com>
2023-12-03 16:23:19 +0100

    3b

--- 2023/3.py
+++ 2023/3.py
@@ -38,20 +38,44 @@ def numbers(grid: Dict[Tuple[int, int], str]) -> Iterable[Tuple[Tuple[int, int],
                 start_x = None
 
 
+def get_value(grid: Dict[Tuple[int, int], str], indices: Tuple[Tuple[int, int], int]) -> int:
+    (start_x, stop_x), y = indices
+    return int(''.join(grid[(x, y)] for x in range(start_x, stop_x)))
+
+
 grid = parse_grid()
 xs = [x for x, _ in grid.keys()]
 ys = [y for _, y in grid.keys()]
 
+# first star
 def parts(grid: Dict[Tuple[int, int], str]) -> Iterable[int]:
     for (start_x, stop_x), y in numbers(grid):
-        number = int(''.join(grid[(x, y)] for x in range(start_x, stop_x)))
+        number = get_value(grid, start_x, stop_x, y)
         # get all neigbours (including the digits)
         ns = set().union(*(neighbors((x, y)) for x in range(start_x, stop_x)))
         is_part = any(is_symbol(grid.get(n, '.')) for n in ns)
         if is_part:
             yield number
 
+#print(sum(parts(grid)))
 
-print(sum(parts(grid)))
-                
-        
+
+# second start
+all_numbers = list(numbers(grid))
+total = 0
+for p, character in grid.items():
+    if character == '*':
+        ns = set(neighbors(p))
+        # find all adjacent numbers
+        adjacent = []
+        for (start_x, stop_x), y in all_numbers:
+            if any((x, y) in ns for x in range(start_x, stop_x)):
+                adjacent.append(((start_x, stop_x), y))
+            
+        if len(adjacent) == 2:
+            # gear found
+            first, second = adjacent
+            gear_ratio = get_value(grid, first) * get_value(grid, second)
+            total += gear_ratio
+print(total)
+
