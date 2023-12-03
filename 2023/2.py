0d7a82d2a654dae77a2a3380600887709c1611c6
Samuel Carlsson <samuel.carlsson@gmail.com>
2023-12-03 15:30:41 +0100

    2b

--- 2023/2.py
+++ 2023/2.py
@@ -1,6 +1,7 @@
+import math
 import sys
 import re
-from typing import Dict, List, Tuple
+from typing import Dict, Iterable, List, Tuple
 
 
 def parse_draw(draw: str) -> Dict[str, int]:
@@ -24,9 +25,20 @@ pool = {'red': 12, 'green': 13, 'blue': 14}
 def ok(draw: Dict[str, int]) -> bool:
     return all(value <= pool[color] for color, value in draw.items())
 
-games = parse()
+games = list(parse())
+
+# first star
 total = 0
 for game_id, draws in games:
     if all(ok(draw) for draw in draws):
         total += game_id
 print(total)
+
+# second star
+power = 0
+for game_id, draws in games:
+    colors = set().union(*(draw.keys() for draw in draws))
+    cubes = {color: max(draw.get(color, 0) for draw in draws) for color in colors}
+    power += math.prod(cubes.values())
+
+print(power)
