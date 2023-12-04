d2625cd6c04ea8d83078b5f39b6c90c3512e23f6
Samuel Carlsson <samuel.carlsson@gmail.com>
2023-12-04 09:29:28 +0100

    refactor: function to compute matches

--- 2023/4.py
+++ 2023/4.py
@@ -1,27 +1,38 @@
 import sys
 import re
-from typing import Set
+from typing import List, Set
 
 def parse(f):
     PATTERN = r'Card\s+(\d+): ([^|]*)\|(.*)'
-    for line in f:
+    for i, line in enumerate(f):
         match = re.match(PATTERN, line)
         if not match:
-            print(line)
+            print('bad line:', line.rstrip())
             continue
         card_id, numbers, winners = match.groups()
-        
+        assert int(card_id) - 1 == i
+
         yield (
-            int(card_id),
             set(int(number) for number in numbers.split()),
             set(int(winner) for winner in winners.split()),
         )
 
 
-def value(numbers: Set[int], winners: Set[int]) -> int:
-    count = sum(1 for number in numbers if number in winners)
-    if count == 0:
+def value(matches: int) -> int:
+    if matches == 0:
         return 0
-    return 2 ** (count - 1)
+    return 2 ** (matches - 1)
 
-print(sum(value(numbers, winners) for _, numbers, winners in parse(sys.stdin)))
+
+def matching(numbers, winners) -> int:
+    return sum(1 for number in numbers if number in winners)
+
+cards = list(parse(sys.stdin))
+matches = [matching(numbers, winners) for numbers, winners in cards]
+
+# first star
+print(sum(value(m) for m in matches))
+
+# second star
+
+#def count(cards: List[int])
