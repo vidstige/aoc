6048f36933254e296b8dbde8dc5029a4ff0e6f0e
Samuel Carlsson <samuel.carlsson@gmail.com>
2024-12-03 08:06:33 +0100

    3

--- 2024/3.py
+++ 2024/3.py
@@ -2,7 +2,15 @@ import re
 import sys
 
 program = sys.stdin.read()
-pattern = re.compile(r'mul\((\d+),(\d+)\)')
+pattern = re.compile(r"mul\((\d+),(\d+)\)|(don't|do)")
 
-total = sum(int(a) * int(b) for a, b in re.findall(pattern, program))
+total = 0
+enabled = True
+for a, b, op in re.findall(pattern, program):
+    if op == "do":
+        enabled = True
+    if op == "don't":
+        enabled = False
+    if enabled and a and b:
+        total += int(a) * int(b)
 print(total)
