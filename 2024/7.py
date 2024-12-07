4d7468d4c97075bcec888f8076a1b1727e046458
Samuel Carlsson <samuel.carlsson@gmail.com>
2024-12-07 13:08:22 +0100

    7b

--- 2024/7.py
+++ 2024/7.py
@@ -7,27 +7,36 @@ def parse(f):
         values = [int(v) for v in parts]
         yield values, int(raw_test_value)
 
-def pop_two(items: list):
-    copy = items.copy()
-    a = copy.pop(0)
-    b = copy.pop(0)
-    return a, b, copy
+def add(a: int, b: int) -> int:
+    return a + b
 
-def search(values: list[int], test_value: int):
+def mul(a: int, b: int) -> int:
+    return a * b
+
+def join(a: int, b: int) -> int:
+    return int(str(a) + str(b))
+
+def search(values: list[int], test_value: int, operators: list):
     if len(values) == 1:
         return test_value == values[0]
-    # add
-    a, b, add = pop_two(values)
-    add.insert(0, a + b)
-    # mul
-    a, b, mul = pop_two(values)
-    mul.insert(0, a * b)
 
-    return search(add, test_value) or search(mul, test_value)
-    
-total = 0
+    results = []
+    for operator in operators:
+        rest = values.copy()
+        a = rest.pop(0)
+        b = rest.pop(0)
+        rest.insert(0, operator(a, b))
+        results.append(search(rest, test_value, operators))
+
+    return any(results)
+
+
+a, b = 0, 0
 for values, test_value in parse(sys.stdin):
-    if search(values, test_value):
-        total += test_value
+    if search(values, test_value, (add, mul)):
+        a += test_value
+    if search(values, test_value, (add, mul, join)):
+        b += test_value
 
-print(total)
+print(a)
+print(b)
